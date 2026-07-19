2e79823b2636f07a89ab4be63b92dac6fa7d84bc
Ismail Larhrib <ilarhrib@e3r9p6.1337.ma>
2026-07-19 12:15:27 +0100

    done

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-# Ensure the PID directory exists for PHP-FPM
+# creating the dir where the .pid will be stored
 mkdir -p /run/php
 
 # Check if the WordPress configuration file already exists in the shared volume
 if [ ! -f /var/www/wordpress/wp-config.php ]; then
     echo "Configuring WordPress..."
 
-    # Download core WordPress files
-    wp core download --allow-root --path=/var/www/wordpress
+    # Download core WordPress files and allowing root so it wont refuse since it refuses to download as root (the container is in root mode)
+    wp core download --allow-root --path=/var/www/wordpress # downloading it in the shared volume between nginx and wp
 
     # Generate the configuration file linked to the MariaDB container
     wp config create --allow-root \
         --dbname=$SQL_DATABASE \
         --dbuser=$SQL_USER \
         --dbpass=$SQL_PASSWORD \
-        --dbhost=mariadb:3306 --path=/var/www/wordpress
+        --dbhost=mariadb:3306 --path=/var/www/wordpress # values will be fetched fro the .env file and we used dns name instead of ip
 
-    # Execute the database population and administrative user creation
+    # configuring the wordpress table in the mariadb 
     wp core install --allow-root \
         --url=ilarhrib.42.fr \
         --title="Inception" \
         --admin_user=$SQL_USER \
         --admin_password=$SQL_PASSWORD \
         --admin_email=admin@ilarhrib.42.fr \
-        --path=/var/www/wordpress
+        --path=/var/www/wordpress # configuring the website url, its title, admin user, its password , email and the path where wp will install 
 
-    # Create the secondary standard user required by the rubric
+    # Create the secondary standard user with no admin power
     wp user create --allow-root $WP_USER_LOGIN $WP_USER_EMAIL \
         --user_pass=$WP_USER_PASSWORD --role=author \
         --path=/var/www/wordpress
